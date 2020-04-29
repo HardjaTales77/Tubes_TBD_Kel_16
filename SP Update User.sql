25fc2802f46941ad750cc7b7758823296dd7b13e
Christophersipus <49676071+Christophersipus@users.noreply.github.com>
2020-04-29 19:32:05 +0700

    Update SP Update User.sql

--- SP Update User.sql
+++ SP Update User.sql
@@ -2,6 +2,42 @@ ALTER PROCEDURE updateUser (
 	@idU int,
 	@namaUser varchar(40)
 ) as
-	UPDATE Users
-	SET Nama_user = @namaUser
-	WHERE IdU = @idU
+	DECLARE @res varchar(60)
+
+	DECLARE @stats int
+	SET @stats = 0
+
+	DECLARE curCariUser CURSOR
+	FOR
+		SELECT Nama_user
+		FROM Users
+	
+	OPEN curCariUser
+		DECLARE @temp varchar(40)
+		FETCH NEXT FROM curCariUser INTO @temp
+
+		WHILE(@@FETCH_STATUS=0)
+		BEGIN
+			IF(@temp != @namaUser)
+			BEGIN
+				SET @stats = 1
+			END
+		END
+
+	CLOSE curCariUser
+	DEALLOCATE curCariUser
+
+	IF(@stats = 1)
+	BEGIN
+		UPDATE Users
+		SET Nama_user = @namaUser
+		WHERE IdU = @idU
+		
+		SET @res = 'Update user dengan nama '+@namaUser+' telah berhasil'
+	END
+	ELSE
+	BEGIN
+		SET @res = 'Nama sudah terdaftar di keanggotaan'
+	END
+
+	print(@res)
