0ce723d94c051e89d0f5b5af0cfef27019193bfd
Christophersipus <49676071+Christophersipus@users.noreply.github.com>
2020-04-29 17:46:23 +0700

    Update Function Cari Id User.sql

--- Function Cari Id User.sql
+++ Function Cari Id User.sql
@@ -1,6 +1,6 @@
 /*
-- Function ini digunakan untuk mencari Id User
-- Input Function ini merupakan nama dari User
+- Function ini digunakan untuk mencari Id Dari seornang User/Anggota
+- Input Function ini memiliki parameter masukan berupa nama dari dari seorang User/Anggota
 */
 
 ALTER FUNCTION cariIdUser
