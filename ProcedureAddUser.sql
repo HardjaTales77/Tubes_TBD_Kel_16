6351040f643ddf6ce2b1f26b6a5f20a8cf31d011
cl1f <56899358+cl1f@users.noreply.github.com>
2020-04-29 19:35:23 +0700

    tambah cek user udah ada apa belom

--- ProcedureAddUser.sql
+++ ProcedureAddUser.sql
@@ -1,5 +1,5 @@
 /*
-		Merupakan fungsi untuk menambah user ke dalam tabel users
+		Merupakan fungsi untuk menambah user ke dalam tabel users, dengan asumsi tidak ada nama yang sama
 		Masukan : Nama User
 */
 
@@ -11,8 +11,39 @@ SET @jumlah=(
 SELECT COUNT(*) FROM Users
 )
 
-INSERT INTO Users
-SELECT @jumlah+1,@Nama_user
-go
+DECLARE curUser CURSOR
+	FOR
+	SELECT Nama_user
+	FROM Users
 
+	OPEN curUser
+
+	DECLARE @varUser varchar(40)
+	DECLARE @tanda int
+	SET @tanda=1
+	FETCH NEXT FROM curUser INTO @varUser
+
+	WHILE(@@FETCH_STATUS=0)
+	BEGIN
+		if(@Nama_user=@varUser)
+		BEGIN
+			SET @tanda=0
+		END
+
+		FETCH NEXT FROM curUser INTO @varUser
+	END
+
+	CLOSE curUser
+	DEALLOCATE curUser
+
+	if(@tanda=1)
+	BEGIN
+		INSERT INTO Users
+		SELECT @jumlah+1,@Nama_user
+	END
+go
+/*
 exec addUser Udin
+
+select * from Users
+*/
