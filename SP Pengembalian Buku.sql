425d9f5db9492143c6ef62509a423d9a88c6c252
Christophersipus <49676071+Christophersipus@users.noreply.github.com>
2020-04-29 13:57:32 +0700

    Update SP Pengembalian Buku.sql

--- SP Pengembalian Buku.sql
+++ SP Pengembalian Buku.sql
@@ -1,33 +1,62 @@
 ALTER PROCEDURE pengembalianBuku
 (
 	@namaUser varchar(40),
-	@idEksemplar int
+	@judulBuku varchar(100)
 ) as
 	DECLARE @res varchar(50)
-
-	DECLARE @temp table(
-		IdU int,
-		IdE int,
-		waktuPinjam date,
-		waktuPengembalian date
-	)
 
 	DECLARE @idUser int
 	SET @idUser = dbo.cariIdUser(@namaUser)
 
-	INSERT INTO @temp
-	SELECT *
-	FROM Meminjam
-	WHERE IdU = @idUser AND IdE = @idEksemplar
+	DECLARE @idBuku int
+	SET @idBuku = dbo.cariIdBuku(@judulBuku)
+
+	DECLARE @listEksemplar table(
+		IdE int
+	)
+	
+	INSERT INTO @listEksemplar
+	SELECT IdE
+	FROM Eksemplar
+	WHERE IdB = @idBuku AND Status = 0
+
+	DECLARE @bukuDipinjam table(
+		IdE int,
+		waktuPinjam Date
+	)
+
+	INSERT INTO @bukuDipinjam
+	SELECT listEkse.IdE, Meminjam.waktuPinjam
+	FROM Meminjam INNER JOIN @listEksemplar as listEkse
+	ON Meminjam.IdE = listEkse.IdE
+
+	DECLARE @idEksemplar int
+	SET @idEksemplar = (
+		SELECT IdE
+		FROM @bukuDipinjam
+		WHERE waktuPinjam = (
+			SELECT MAX(waktuPinjam)
+			FROM @bukuDipinjam
+		)
+	)
 
 	UPDATE Meminjam
 	SET waktuPengembalian = CONVERT(DATE,GETDATE())
 	WHERE waktuPinjam = (
 		SELECT MAX(waktuPinjam)
-		FROM @temp
+		FROM @bukuDipinjam
 	) AND IdE = @idEksemplar
+
 	UPDATE Eksemplar
 	SET Status = 1
 	WHERE IdE = @idEksemplar
 
-exec pengembalianBuku 'John Doe',2
+exec pengembalianBuku 'John Doe','Harry Potter and The Philosopher Stone'
+
+select *
+from Meminjam
+/*Asumsi-asumsi
+- Orang tidak akan mengembalikan buku yang tidak di pinjam
+- Orang tidak akan meminjam 2 eksemplar dengan judul buku yang sama
+- User yang meminjam pasti terdaftar di tabel user
+*/
